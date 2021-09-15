298e9b018764d27609fbd5e8b6312b9815ca93ab
iamilam <ipsoftcreationsyt0614@gmail.com>
2021-09-15 19:41:28 +0530

    haha github desktop go br

---
+++ src/main/java/com/pseudosmp/PlayerBiomes/PlayerBiomes.java
@@ -0,0 +1,25 @@
+package com.pseudosmp.PlayerBiomes;
+
+import de.jeff_media.jefflib.*;
+import de.jeff_media.jefflib.pluginhooks.PlaceholderAPIUtils;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class PlayerBiomes extends JavaPlugin  {
+    @Override
+    public void onEnable() {
+        System.out.println("Disabling PlayerBiomes...");
+        JeffLib.init(this);
+        PlaceholderAPIUtils.register("biome", player -> {
+            if(player.isOnline()) {
+                return BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
+            } else {
+                return null;
+            }
+        });
+    }
+    @Override
+    public void onDisable(){
+        System.out.println("Disabling PlayerBiomes...");
+    }
+}
