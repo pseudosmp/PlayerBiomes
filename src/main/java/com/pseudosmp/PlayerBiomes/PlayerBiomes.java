70e92106d4d440287e3b2eebe9cd10f790c8530a
iamilam <ipsoftcreationsyt0614@gmail.com>
2021-09-16 11:51:22 +0530

    [Credit: Si6gma#0828] Capitalised biome names, Support terralith reserved biomes

--- src/main/java/com/pseudosmp/PlayerBiomes/PlayerBiomes.java
+++ src/main/java/com/pseudosmp/PlayerBiomes/PlayerBiomes.java
@@ -16,6 +16,32 @@ public class PlayerBiomes extends JavaPlugin  {
                 return null;
             }
         });
+        PlaceholderAPIUtils.register("biome_capitalized", player -> {
+            if (player.isOnline()) {
+                String biome = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
+                int findSlash = biome.indexOf('/');
+
+                // Checks if it has a '/', if yes it removes everything before it.
+                // For Terralith reserved biomes, you dont need to worry about this unless any one of your biome names has a forward slash
+                if (findSlash != -1) {
+                    biome = biome.substring(findSlash + 1);
+                    findSlash = biome.indexOf('/');
+                    biome = biome.substring(findSlash + 1);
+                } else {
+                }
+
+                // Capitalizes first letter in both words.
+                int findUnderScore = biome.indexOf("_");
+                biome = biome.substring(0, 1).toUpperCase() + biome.substring(1, findUnderScore)
+                        + biome.substring(findUnderScore, findUnderScore + 2).toUpperCase()
+                        + biome.substring(findUnderScore + 2);
+                biome = biome.replace("_", " ");
+
+                return biome;
+            } else {
+                return null;
+            }
+        });
     }
     @Override
     public void onDisable(){
