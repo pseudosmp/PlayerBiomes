4562751ca0c3bd095195c62024117de471a58ee8
Ilamparithi M <5023573ilamparithi@gmail.com>
2023-02-19 23:23:09 +0530

    Add the two new placeholders and rename the two existing ones

--- src/main/java/com/pseudosmp/PlayerBiomes/PlayerBiomes.java
+++ src/main/java/com/pseudosmp/PlayerBiomes/PlayerBiomes.java
@@ -10,9 +10,25 @@ public class PlayerBiomes extends JavaPlugin {
     public void onEnable() {
         System.out.println("Load Successful!");
         JeffLib.init(this);
-        PlaceholderAPIUtils.register("biome", player -> {
+        PlaceholderAPIUtils.register("biome_raw", player -> {
             if(player.isOnline()) {
-                return BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getNamespace() + ";" + BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
+                return BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getNamespace() + ":" + BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
+            } else {
+                return null;
+            }
+        });
+        PlaceholderAPIUtils.register("biome_namespace", player -> {
+            if(player.isOnline()) {
+                String ns = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getNamespace();
+                return ns.substring(0, 1).toUpperCase() + ns.substring(1);
+            } else {
+                return null;
+            }
+        });
+        PlaceholderAPIUtils.register("biome_name", player -> {
+            if(player.isOnline()) {
+                String n = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
+                return n.substring(0, 1).toUpperCase() + n.substring(1);
             } else {
                 return null;
             }
@@ -37,10 +53,7 @@ public class PlayerBiomes extends JavaPlugin {
                 }
 
                 String words[] = biome.split("\\s");
-
                 for (String w : words) {
-                    //String first = w.substring(0, 1);
-                    //String afterfirst = w.substring(1);
                     formattedBiome += w.substring(0, 1).toUpperCase() + w.substring(1) + " ";
                 }
                 formattedBiome = biomeNamespace.substring(0, 1).toUpperCase() + biomeNamespace.substring(1) + ": " + formattedBiome;
