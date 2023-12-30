c7fe72dbe6f65fdb05214c7844c1eff4d372793a
Ilamparithi M <5023573ilamparithi@gmail.com>
2023-12-30 11:30:40 +0530

    backport literally everything

--- src/main/java/com/pseudoforce/PlayerBiomes/PlayerBiomes.java
+++ src/main/java/com/pseudoforce/PlayerBiomes/PlayerBiomes.java
@@ -2,16 +2,51 @@ package com.pseudoforce.PlayerBiomes;
 
 import com.jeff_media.jefflib.*;
 import com.jeff_media.jefflib.pluginhooks.PlaceholderAPIUtils;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.bstats.bukkit.Metrics;
 
 
 public class PlayerBiomes extends JavaPlugin {
+
+    public static String getBiomeFormatted(OfflinePlayer player) {
+        NamespacedKey namespacedKey = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation());
+        String biomeKey = namespacedKey.getKey();
+        String biomeNamespace = namespacedKey.getNamespace();
+
+        String biome = biomeKey.replaceAll("_", " ");
+
+        int findSlash = biome.indexOf("/");
+        if (findSlash != -1) {
+            biome = biome.substring(biome.lastIndexOf("/") + 1);
+        }
+
+        String formattedBiome = Arrays.stream(biome.split("\\s"))
+            .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
+            .collect(Collectors.joining(" "));
+
+        formattedBiome = biomeNamespace.substring(0, 1).toUpperCase() + biomeNamespace.substring(1) + ": " + formattedBiome;
+
+        return formattedBiome.trim();
+    }
+
     @Override
     public void onEnable() {
-        // bstats
-        int pluginId = 17782;
-        Metrics metrics = new Metrics(this, pluginId);
+        
+        // bstats, enabled by default 
+        if (getConfig().getBoolean("bstats_consent", true)) {
+            int pluginId = 17782;
+            Metrics metrics = new Metrics(this, pluginId);
+        }
         
         // placeholders
         JeffLib.init(this);
@@ -34,25 +69,19 @@ public class PlayerBiomes extends JavaPlugin {
 
         PlaceholderAPIUtils.register("biome_name", player -> {
             if(player.isOnline()) {
-                String biome = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
-                String n = "";
-
-                biome = biome.replaceAll("_", " ");
-
+                String biomeKey = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
+                String biome = biomeKey.replaceAll("_", " ");
+            
                 int findSlash = biome.indexOf("/");
                 if (findSlash != -1) {
-                    do {
-                        biome = biome.substring(findSlash + 1);
-                        findSlash = biome.indexOf("/");
-                    } while (findSlash != -1);
+                    biome = biome.substring(biome.lastIndexOf("/") + 1);
                 }
-
-                String words[] = biome.split("\\s");
-                for (String w : words) {
-                    n += w.substring(0, 1).toUpperCase() + w.substring(1) + " ";
-                }
-
-                return n.trim();
+            
+                String formattedBiomeName = Arrays.stream(biome.split("\\s"))
+                    .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
+                    .collect(Collectors.joining(" "));
+            
+                return formattedBiomeName.trim();            
             } else {
                 return null;
             }
@@ -60,31 +89,47 @@ public class PlayerBiomes extends JavaPlugin {
 
         PlaceholderAPIUtils.register("biome_formatted", player -> {
             if (player.isOnline()) {
-                String biomeKey = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getKey();
-                String biomeNamespace = BiomeUtils.getBiomeNamespacedKey(player.getPlayer().getLocation()).getNamespace();
-                String biome = "";
-                String formattedBiome = "";
-
-                biome = biomeKey.replaceAll("_", " ");
-                
-                int findSlash = biome.indexOf("/");
-
-                if (findSlash != -1) {
-                    do {
-                        biome = biome.substring(findSlash + 1);
-                        findSlash = biome.indexOf("/");
-                    } while (findSlash != -1);
-                }
-
-                String words[] = biome.split("\\s");
-                for (String w : words) {
-                    formattedBiome += w.substring(0, 1).toUpperCase() + w.substring(1) + " ";
-                }
-                formattedBiome = biomeNamespace.substring(0, 1).toUpperCase() + biomeNamespace.substring(1) + ": " + formattedBiome;
-                return formattedBiome.trim();
+                return getBiomeFormatted(player);
             } else {
                 return null;
             }
         });
+        // Save the default config if it doesn't exist
+        this.saveDefaultConfig();
+
+        // Register the /whereami command
+        this.getCommand("whereami").setExecutor(new WhereAmICommand(this));
+        getLogger().info("/whereami is now a valid question! (PlayerBiomes has been enabled)");
+    }
+    public class WhereAmICommand implements CommandExecutor {
+        private final JavaPlugin plugin;
+        public WhereAmICommand(JavaPlugin plugin) {
+            this.plugin = plugin;
+        }
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            if (!(sender instanceof Player)) {
+                String message = plugin.getConfig().getString("messages.console_whereami");
+                if (message == null) {
+                    getLogger().warning("The console message for /whereami is blank. Check your config!");
+                    message = "This command can only be executed by a player.";
+                }
+                sender.sendMessage(message);
+                return true;
+            }
+
+            OfflinePlayer player = (Player) sender;
+            String formattedBiome = getBiomeFormatted(player);
+
+            // Get the message from the config and send
+            String message = plugin.getConfig().getString("messages.user_whereami");
+            if (message == null) {
+                getLogger().warning("The user message for /whereami is blank. Check your config!");
+                message = "You are currently in the %s biome.";
+            }
+            player.getPlayer().sendMessage(String.format(message, formattedBiome));
+            return true;
+        }
     }
 }
