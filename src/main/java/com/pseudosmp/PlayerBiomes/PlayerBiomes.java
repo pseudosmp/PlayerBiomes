5c448ed2b1d5ddd2237190f3def17abed9702310
Ilamparithi Murali <ilamparithi.in@gmail.com>
2025-04-22 01:30:36 +0530

    Adding debug string to check modernbiomeapi

--- src/main/java/com/pseudosmp/PlayerBiomes/PlayerBiomes.java
+++ src/main/java/com/pseudosmp/PlayerBiomes/PlayerBiomes.java
@@ -43,6 +43,7 @@ public class PlayerBiomes extends JavaPlugin {
 
                 Object namespacedKeyObj = biome.getClass().getMethod("getKeyOrThrow").invoke(biome);
                 namespacedKey = (NamespacedKey) namespacedKeyObj;
+                System.out.println("[DEBUG] Using Modern Biome API - NamespacedKey: " + namespacedKey.toString());
 
             } catch (Throwable t) {
                 t.printStackTrace();
